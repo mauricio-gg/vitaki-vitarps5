e204aed617f789c948590a59d389183ba5eb59c5
Mauricio G. <59623310+mauricio-gg@users.noreply.github.com>
2025-11-18 21:51:17 +0100

    Revert "Include timestamp in Chiaki controller seq logs"
    This reverts commit 7b8314d1598bda1f330e701c978c9d21bdf9514c.

--- lib/src/feedbacksender.c
+++ lib/src/feedbacksender.c
@@ -280,11 +280,8 @@ static void *feedback_sender_thread_func(void *user)
 			feedback_sender_send_state(feedback_sender);
 			feedback_sender->controller_seq_counter++;
 			if((feedback_sender->controller_seq_counter % 500) == 0) {
-				uint64_t now_us = chiaki_time_now();
 				CHIAKI_LOGI(feedback_sender->log,
-						"Controller send seq %u (Chiaki) time %llu",
-						feedback_sender->controller_seq_counter,
-						(unsigned long long)now_us);
+						"Controller send seq %u (Chiaki)", feedback_sender->controller_seq_counter);
 			}
 		}
 
