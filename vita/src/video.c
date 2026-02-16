8d8222f45cb6680779a9b00dbddef89a9f00a87e
Mauricio G. <59623310+mauricio-gg@users.noreply.github.com>
2026-02-16 23:03:40 +0100

    Continue cleanup: deflate video.c dead legacy paths

--- vita/src/video.c
+++ vita/src/video.c
@@ -22,8 +22,6 @@
 #include "context.h"
 #include "ui.h"
 
-#include <h264-bitstream/h264_stream.h>
-
 #include <chiaki/thread.h>
 
 #include <stdbool.h>
@@ -46,7 +44,6 @@ enum {
   VITA_VIDEO_ERROR_ALLOC_MEM            = 0x80010004,
   VITA_VIDEO_ERROR_GET_MEMBASE          = 0x80010005,
   VITA_VIDEO_ERROR_CREATE_DEC           = 0x80010006,
-  VITA_VIDEO_ERROR_CREATE_PACER_THREAD  = 0x80010007,
 };
 
 enum {
@@ -64,7 +61,6 @@ enum VideoStatus {
   INIT_AVC_LIB,
   INIT_DECODER_MEMBLOCK,
   INIT_AVC_DEC,
-  INIT_FRAME_PACER_THREAD,
 };
 
 vita2d_texture *frame_texture = NULL;
@@ -202,10 +198,6 @@ ChiakiMutex mtx;
 bool threadSetupComplete = false;
 
 void vita_h264_cleanup() {
-	if (video_status == INIT_FRAME_PACER_THREAD) {
-		video_status--;
-	}
-
 	if (video_status == INIT_AVC_DEC) {
 		sceAvcdecDeleteDecoder(decoder);
 		video_status--;
@@ -284,8 +276,6 @@ SceAvcdecArrayPicture array_picture = {0};
 struct SceAvcdecPicture picture = {0};
 struct SceAvcdecPicture *pictures = { &picture };
 
-
-bool first_frame = false;
 int vita_h264_setup(int width, int height) {
   int ret;
   LOGD("vita video setup\n");
@@ -293,7 +283,6 @@ int vita_h264_setup(int width, int height) {
 	SceVideodecMemInfo libMemInfo;
   SceVideodecQueryInitInfo initVideodec;
 	void *libMem;
-  first_frame = true;
 
   array_picture.numOfElm = 1;
   array_picture.pPicture = &pictures;
@@ -312,8 +301,6 @@ int vita_h264_setup(int width, int height) {
   }
 
   if (video_status == INIT_GS) {
-    // INIT_FRAMEBUFFER
-    // update_scaling_settings(SCREEN_WIDTH, SCREEN_HEIGHT);
     update_scaling_settings(width, height);
     picture.frame.framePitch = image_scaling.texture_width;
     picture.frame.frameWidth = image_scaling.texture_width;
@@ -332,14 +319,6 @@ int vita_h264_setup(int width, int height) {
   }
 
   if (video_status == INIT_FRAMEBUFFER) {
-    // INIT_AVC_LIB
-    // if (initVideodec == NULL) {
-          // if (init == NULL) {
-        // LOGD("not enough memory1\n");
-        // ret = VITA_VIDEO_ERROR_NO_MEM;
-        // goto cleanup;
-      // }
-    // }
     sceClibMemset(&initVideodec.hwAvc, 0, sizeof(SceVideodecQueryInitInfoHwAvcdec));
 
 		initVideodec.hwAvc.size = sizeof(SceVideodecQueryInitInfoHwAvcdec);
@@ -405,7 +384,6 @@ int vita_h264_setup(int width, int height) {
   }
 
   if (video_status == INIT_AVC_LIB) {
-    // INIT_DECODER_MEMBLOCK
     if (decoder_info == NULL) {
       decoder_info = calloc(1, sizeof(SceAvcdecQueryDecoderInfo));
       if (decoder_info == NULL) {
@@ -442,7 +420,6 @@ int vita_h264_setup(int width, int height) {
 		opt.attr = 4;
 		opt.alignment = 1024 * 1024;
     decoderblock = sceKernelAllocMemBlock("decoder", SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW, decoder_info_out.frameMemSize, &opt);
-    // decoderblock = sceKernelAllocMemBlock("decoder", SCE_KERNEL_MEMBLOCK_TYPE_USER_MAIN_PHYCONT_NC_RW, sz, NULL);
     if (decoderblock < 0) {
       LOGD("decoderblock: 0x%08x\n", decoderblock);
       ret = VITA_VIDEO_ERROR_ALLOC_MEM;
@@ -459,7 +436,6 @@ int vita_h264_setup(int width, int height) {
   }
 
   if (video_status == INIT_DECODER_MEMBLOCK) {
-    // INIT_AVC_DEC
     LOGD("base: 0x%08x\n", decoder->frameBuf.pBuf);
 
     ret = sceAvcdecCreateDecoder(SCE_VIDEODEC_TYPE_HW_AVCDEC, decoder, decoder_info);
@@ -471,11 +447,6 @@ int vita_h264_setup(int width, int height) {
     video_status++;
   }
 
-  if (video_status == INIT_AVC_DEC) {
-    // INIT_FRAME_PACER_THREAD
-    video_status++;
-  }
-
   return VITA_VIDEO_INIT_OK;
 
 cleanup:
@@ -483,96 +454,6 @@ cleanup:
   return ret;
 }
 
-// #define GS_SPS_BITSTREAM_FIXUP 0x01
-// #define GS_SPS_BASELINE_HACK 0x02
-
-// https://github.dev/intel-linyonghui/RPiPlay/blob/4a65c7b84ff09acd7d6d494bb9037bbf106153a8/renderers/video_renderer_rpi.c?q=NAL_UNIT_TYPE_SPS & https://github.dev/Stary2001/moonlight-embedded/blob/04a7c1d8b24571f2bc7807894c2db8b8096b0c33/libgamestream/sps.c
-bool vita_h264_process_header(uint8_t *data, size_t data_len) {
-  // uint8_t *modified_data;
-  // This reduces the Raspberry Pi H264 decode pipeline delay from about 11 to 6 frames for RPiPlay.
-  // Described at https://www.raspberrypi.org/forums/viewtopic.php?t=41053
-  int sps_start, sps_end;
-  int sps_size = find_nal_unit(data, data_len, &sps_start, &sps_end);
-  if (sps_size > 0) {
-      LOGD("replacing SPS");
-      // const int sps_wiggle_room = 12;
-      // const unsigned char nal_marker[] = { 0x0, 0x0, 0x0, 0x1 };
-      // int modified_data_len = *data_len + sps_wiggle_room + sizeof(nal_marker);
-      // uint8_t* modified_data = malloc(data_len);
-
-      h264_stream_t *h = h264_new();
-      int actual_sps_size = read_nal_unit(h, &data[sps_start], sps_size);
-      if (actual_sps_size < 0) {
-        LOGD("Reading NAL error %d", actual_sps_size);
-        return false;
-      }
-      // h->nal->nal_unit_type = NAL_UNIT_TYPE_SPS;
-      h->sps->num_ref_frames = REF_FRAMES;
-      // h->sps->level_idc = 32; // Max 5 buffered frames at 1280x720x60
-      // h->sps->vui.bitstream_restriction_flag = 1;
-      // h->sps->vui.max_bits_per_mb_denom = 1;
-      // h->sps->vui.log2_max_mv_length_horizontal = 16;
-      // h->sps->vui.log2_max_mv_length_vertical = 16;
-      // h->sps->vui.num_reorder_frames = 0;
-      // h->sps->profile_idc = H264_PROFILE_BASELINE;
-
-      // h->sps->vui.motion_vectors_over_pic_boundaries_flag = 1;
-      // h->sps->vui.matrix_coefficients = 0;
-
-      // h->sps->log2_max_frame_num_minus4 = 4;
-      // h->sps->vui.aspect_ratio_idc = 255;
-
-      // h->sps->frame_cropping_flag = 0;
-      // h->sps->frame_crop_bottom_offset = 0;
-      // h->sps->level_idc = 31;
-      // h->sps->profile_idc = 100;
-      // h->sps->constraint_set1_flag = 0;
-      // h->sps->vui.log2_max_mv_length_horizontal = 1;
-      // h->sps->vui.log2_max_mv_length_vertical = 1;
-
-      // h->sps->vui.num_units_in_tick = 1000;
-      // h->sps->vui.time_scale = 60000;
-
-
-      // GFE 2.5.11 changed the SPS to add additional extensions
-      // Some devices don't like these so we remove them here.
-      // h->sps->vui.video_signal_type_present_flag = 0;
-      // h->sps->vui.chroma_loc_info_present_flag = 0;
-
-      // // Some devices throw errors if max_dec_frame_buffering < num_ref_frames
-      h->sps->vui.max_dec_frame_buffering = REF_FRAMES;
-
-      // // These values are the default for the fields, but they are more aggressive
-      // // than what GFE sends in 2.5.11, but it doesn't seem to cause picture problems.
-      // h->sps->vui.max_bytes_per_pic_denom = 2;
-      // h->sps->vui.max_bits_per_mb_denom = 1;
-
-      LOGD("sps real type %d type %d starts at 0x%x ends at 0x%x length 0x%x buf size 0x%x sps size 0x%x actual size 0x%x", (*(data + sps_start) & 0x1F), h->nal->nal_unit_type, sps_start, sps_end, data_len - sps_start, data_len, sps_size, actual_sps_size);
-      int new_sps_size = write_nal_unit(h, data + sps_start, data_len); // i dont know why we need data_len instead of sps_size but it works and doesnt destroy it lel
-      LOGD("new size 0x%x or %d", new_sps_size, new_sps_size);
-      // bool ret;
-      // if (new_sps_size > 0 && new_sps_size <= sps_wiggle_room) {
-      //     memcpy(modified_data, *data, sps_start);
-      //     memcpy(modified_data + sps_start + new_sps_size, nal_marker, sizeof(nal_marker));
-      //     memcpy(modified_data + sps_start + new_sps_size + sizeof(nal_marker), *data + sps_start, *data_len - sps_start);
-      //     *data = modified_data;
-      //     *data_len = *data_len + new_sps_size + sizeof(nal_marker);
-      //     ret = true;
-      // } else {
-      //     free(modified_data);
-      //     modified_data = NULL;
-      //     ret = false;
-      // }
-      h264_free(h);
-      return true;
-  } else {
-    LOGD("cant find SPS");
-    return false;
-  }
-}
-
-// uint8_t *lbuf;
-// bool infirst_frame = false;
 int vita_h264_decode_frame(uint8_t *buf, size_t buf_size) {
   // Early validation to detect corrupted frames before decoding
   if (buf == NULL || buf_size == 0) {
@@ -586,44 +467,12 @@ int vita_h264_decode_frame(uint8_t *buf, size_t buf_size) {
     return 1;
   }
 
-  // free(lbuf);
-  // lbuf = buf;
   chiaki_mutex_lock(&mtx);
   if(!threadSetupComplete) {
 		sceKernelChangeThreadPriority(SCE_KERNEL_THREAD_ID_SELF, 64);
 		sceKernelChangeThreadCpuAffinityMask(SCE_KERNEL_THREAD_ID_SELF, SCE_KERNEL_CPU_MASK_USER_0);
 		threadSetupComplete = true;
 	}
-  // if (first_frame) {
-  //   first_frame = false;
-  //   // infirst_frame = true;
-  //   // header storage removed
-  //   //   LOGD("not enough memory for header buf\n");
-  //   //   return 1;
-  //   // }
-
-  //   // header_size = buf_size;
-  //   // sceClibMemcpy(header_storage, buf, buf_size);
-
-  //   vita_h264_process_header(buf, buf_size);
-  //   // header_storage = buf;
-  //   // header_size = buf_size;
-  //   // hexdump(buf, buf_size);
-
-  //   // chiaki_mutex_unlock(&mtx);
-  //   // return 0;
-  // }
-
-
-
-  // hexdump(buf, buf_size);
-
-
-
-  //frame->time = decodeUnit->receiveTimeMs;
-
-
-
   if (buf_size > sceAvcdecDecodeAvailableSize(decoder)) {
     sceClibPrintf("Video decode buffer too small\n");
     chiaki_mutex_unlock(&mtx);
@@ -632,46 +481,6 @@ int vita_h264_decode_frame(uint8_t *buf, size_t buf_size) {
 
 
   int ret = 0;
-  // if (frame_count != 0 && frame_count % 6 == 0) {
-  //   LOGD("flushing decoder");
-  //   sceAvcdecDecodeFlush(decoder);
-  //   au.es.pBuf = header_storage;
-  //   au.es.size = header_size;
-
-  //   ret = sceAvcdecDecode(decoder, &au, &array_picture);
-  //   if (ret < 0) {
-  //     LOGD("sceAvcdecDecodeHeaderBuf (len=0x%x): 0x%x numOfOutput %d\n", buf_size, ret, array_picture.numOfOutput);
-  //     chiaki_mutex_unlock(&mtx);
-  //     return 1; // decoder screwed up for some reason, just ignore this frame
-  //   }
-  //   return 1;
-  // }
-
-  // PLENTRY entry = decodeUnit->bufferList;
-  // uint32_t length = 0;
-  // while (entry != NULL) {
-    // if (entry->bufferType == BUFFER_TYPE_SPS) {
-      // gs_sps_fix(entry, GS_SPS_BITSTREAM_FIXUP, decode_buf, &length);
-    // } else {
-      // sceClibMemset(decode_buf, 0, buffer_size);
-      // memset(buf, buf_size);
-      // free(buf);
-      // decode_buf = buf;
-      // length += entry->length;
-    // }
-    // entry = entry->next;
-  // }
-
-  // au.es.size = buf_size;
-  // au.dts.lower = 0xFFFFFFFF;
-  // au.dts.upper = 0xFFFFFFFF;
-  // au.pts.lower = 0xFFFFFFFF;
-  // au.pts.upper = 0xFFFFFFFF;
-  // sceClibMemcpy(decode_buf, buf, buf_size);
-
-  // sceClibMemcpy(decode_buf, buf, buf_size);
-
-  // au.es.pBuf = decode_buf;
   au.es.pBuf = buf;
   au.es.size = buf_size;
   uint64_t decode_start_us = sceKernelGetProcessTimeWide();
@@ -685,33 +494,14 @@ int vita_h264_decode_frame(uint8_t *buf, size_t buf_size) {
   context.stream.decode_window_count++;
   if (ret < 0) {
     LOGD("sceAvcdecDecode (len=0x%x): 0x%x numOfOutput %d\n", buf_size, ret, array_picture.numOfOutput);
-    // if (isEdited) free(buf);
     chiaki_mutex_unlock(&mtx);
     return 0;
-    // goto fix;
   }
 
   if (array_picture.numOfOutput != 1) {
     LOGD("numOfOutput %d bufSize 0x%x\n", array_picture.numOfOutput, buf_size);
-    // if (infirst_frame) {
-    //   infirst_frame = false;
-    //   // if (isEdited) free(buf);
-    //   chiaki_mutex_unlock(&mtx);
-    //   return 0;
-    // } else {
-      // goto fix;
-      // if (isEdited) free(buf);
-      chiaki_mutex_unlock(&mtx);
-      return 0;
-      // chiaki_mutex_unlock(&mtx);
-      // return 1;
-    // }
-    // if (first_frame) {
-    //   first_frame = false;
-    //   chiaki_mutex_unlock(&mtx);
-    //   return 0;
-    // }
-    // goto fix;
+    chiaki_mutex_unlock(&mtx);
+    return 0;
   }
   // Signal the UI thread that a new frame is ready for display.
   // The UI thread owns all vita2d rendering, which decouples the GPU wait
